acedffb8fb0c86b5ecc57f15867b20474a2164a8
jameshussey474 <jameshussey474@gmail.com>
2023-05-25 16:41:41 +0800

    dev: optimized

--- constants/data.js
+++ constants/data.js
@@ -8,41 +8,41 @@ const userData = {
   address: "2668 Randolph Ave, Windsor OT",
   projects: [
     {
-      id:0,
+      id: 0,
       title: "Big Chain DB",
       link: "https://www.bigchaindb.com/",
       imgUrl: "/BrainDB.png",
     },
     {
-      id:1,
+      id: 1,
       title: "F-Ecommerce",
       link: "https://github.com/jameshussey474",
       imgUrl: "/Fecommerce.png",
     },
     {
-      id:2,
+      id: 2,
       title: "FrontStore",
       link: "https://www.reactstorefront.io/",
       imgUrl: "/FrontStore.png",
     },
     {
-      id:3,
+      id: 3,
       title: "Heroic Labs",
       link: "https://heroiclabs.com/",
       imgUrl: "/HeroicLabs.png",
     },
     {
-      id:4,
+      id: 4,
       title: "Infinitive Lights",
       link: "https://tympanus.net/Tutorials/InfiniteLights/",
       imgUrl: "/Traffic OHIO.jpg",
     },
     {
-      id:5,
+      id: 5,
       title: "Playground",
       link: "https://play.placeholdertech.in",
       imgUrl: "/Wcommerce.png",
-    }
+    },
   ],
   about: {
     title:
@@ -52,12 +52,10 @@ const userData = {
       `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
       `But now, I'm a legend. I've been coding in React, Angular, Vue, Svelte, Node.js, Express, Fastify, MongoDB, MySQL, Bootstrap, Tailwind, C, C++, C#, Rust, Haskell, Perl, PHP, Assembly, Logo and YouDontKnowShit programming language etc to name a few. Although I barely know the syntax (Psst, Stack overflow!), I consider myself a Ninja developer I'm laughing right now.`,
     ],
-    // currentProject: "Tailwind Master Kit",
-    // currentProjectUrl: "https://tailwindmasterkit.com",
   },
   experience: [
     {
-      id:0,
+      id: 0,
       title: "Middle-level Full Stack Web Developer",
       company: "WebFX",
       year: "2021",
@@ -65,7 +63,7 @@ const userData = {
       desc: "WebFX was definitely the thing that give me an extraordinary honour and opportunity to take off!!!",
     },
     {
-      id:1,
+      id: 1,
       title: "Middle-level Full Stack Web Developer",
       company: "Hootsuite",
       year: "2019",
@@ -73,7 +71,7 @@ const userData = {
       desc: "I was promoted to a middle-level position due to my dedication and contributions to my company.",
     },
     {
-      id:2,
+      id: 2,
       title: "Junior Full Stack Web Developer",
       company: "WattPad",
       year: "2019",
@@ -81,7 +79,7 @@ const userData = {
       desc: "Junior Developer! It was started to work as pro developer, created high-quality products.",
     },
     {
-      id:3,
+      id: 3,
       title: "Internship",
       company: "Wattpad",
       year: "2018",
@@ -89,24 +87,45 @@ const userData = {
       desc: "Developed stunning and awesome UIs, focusing on responsive design principle.",
     },
     {
-      id:4,
+      id: 4,
       title: "Graduation",
       company: "The University of Waterloo",
       year: "2018",
       companyLink: "https://upes.co.in",
       desc: "Major in Software Science and Engineering with a CGPA of 7.0. It was first successful journey in my career",
-    }
+    },
   ],
   resumeUrl:
     "https://drive.google.com/file/d/1xmE3BOmgM7TAOOgVp36xQIQvYDntDYoo/view?usp=sharing",
-  socialLinks: {
-    instagram: "https://instagram.com/",
-    twitter: "https://twitter.com/",
-    linkedin: "https://linkedin.com/in/",
-    github: "https://github.com/jameshussey700",
-    facebook: "https://facebook.com/",
-    peerlist: "https://peerlist.io/",
-  },
+  socialLinks: [
+    {
+      name: "instagram",
+      url: "https://instagram.com/",
+    },
+    {
+      name: "twitter",
+      url: "https://twitter.com/",
+    },
+    {
+      name: "linkedin",
+      url: "https://linkedin.com/in/",
+    },
+    {
+      name: "github",
+      url: "https://github.com/jameshussey700",
+    },
+    {
+      name: "facebook",
+      url: "https://facebook.com/",
+    },
+    {
+      name: "peerlist",
+      url: "https://peerlist.io/",
+    },
+  ],
+  skills:[
+    'javascript','typescript','html','css','angular','vue', 'git', 'react','firebase','mongodb'
+  ]
 };
 
 export default userData;
--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,10 @@ import LatestCode from "../components/LatestCode";
 import Hero from "../components/Hero";
 import getLatestRepos from "@lib/getLatestRepos";
 import userData from "@constants/data";
-
 export default function Home({ repositories }) {
   return (
     <ContainerBlock
-      title="Manu Arora - Developer, Writer, Creator"
+      title="James Hussey - Developer, Writer, Creator"
       description="This is a template built specifically for my blog - Creating a developer portfolio that gets you a job."
     >
       <Hero />
@@ -19,14 +18,10 @@ export default function Home({ repositories }) {
     </ContainerBlock>
   );
 }
-
 export const getServerSideProps = async () => {
   console.log(process.env.GITHUB_AUTH_TOKEN);
   let token = process.env.GITHUB_AUTH_TOKEN;
-
   const repositories = await getLatestRepos(userData, token);
-  // console.log("REPOSITORIES", repositories);
-
   return {
     props: {
       repositories,
