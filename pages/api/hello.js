3e6a7cf71716e3bfc7ede401afac01bdeab8c9f6
jameshussey474 <jameshussey474@gmail.com>
2023-05-25 15:02:13 +0800

    dev: runtime mode changed

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+export const runtime = 'experimental-edge';
 export default (req, res) => {
   res.statusCode = 200
   res.json({ name: 'James Hussey' })
